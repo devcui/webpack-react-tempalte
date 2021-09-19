8084cd802aeaead423069bd4e19e1bed77125128
devcui <devcui@outlook.com>
2021-09-19 09:01:20 +0800

    feat(all): first commit

---
+++ scripts/consts.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+const ROOT_PATH = path.resolve(__dirname, '../')
+const SERVER_HOST = "127.0.0.1"
+const SERVER_PORT = 3000;
+
+
+module.exports = {
+    ROOT_PATH,
+    SERVER_HOST,
+    SERVER_PORT
+}
